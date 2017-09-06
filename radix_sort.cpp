9ab86bf9287c62bb4ffb74aeabf03c28f8003bd5
Diego Alejandro Rodriguez Ayala <diego.magno120@gmail.com>
2017-09-06 18:37:08 -0500

    Added practices about radix sort and residuo sort (lol)

---
+++ radix_sort.cpp
@@ -0,0 +1,108 @@
+/**
+  Diego Alejandro Rodriguez Ayala - 20142020068
+  Practica para estudiar el algoritmo de ordenamiento Radix Sort y su comportamiento
+  Ademas se calcula tiempo de ejecución del algoritmo para compararlo despues con el t(n) teorico.
+**/
+
+#include <stdio.h>
+#include <time.h>
+#include "sorting_algorithms.h"
+//#include <windows.h>
+
+using namespace std;
+
+void radixSort(int *array, int n);
+int max(int *array, int n);
+void sorting(int *array, int n, int exp);
+//double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b);
+
+int main(int argc, char const *argv[]) {
+
+  int *array, n;
+  /*
+  double secs = 0;
+  LARGE_INTEGER t_ini, t_fin;
+  FILE *archivo;
+  archivo = fopen("tiempo.txt", "a");
+  */
+  printf("Ingrese el valor de N: ");
+  scanf("%i", &n);
+
+  array = new int[n];
+
+  srand(time(0)); // Planta semilla para generar un random diferente
+
+  for (int i = 0; i < n; i++) {
+    array[i] = 1000+rand()%(10000-1000); // Para 4 cifras
+    //array[i] = 10000000+rand()%(100000000-10000000); // Para 8 cifras
+  }
+
+  printf("\nArray:\n");
+  printArray(array, n);
+
+  // Mide tiempo de ejecución entre &t_ini y &t_fin
+  //QueryPerformanceCounter(&t_ini);
+  radixSort(array, n); // Algoritmo de ordenamiento - Radix Sort
+  //QueryPerformanceCounter(&t_fin);
+
+  printf("\nArray Ordenado:\n");
+  printArray(array, n);
+  /*
+  secs = performancecounter_diff(&t_fin, &t_ini);
+  printf("\nTime: %.16g milliseconds\n", secs*1000.0);
+
+  fprintf(archivo, "\n%.16g", secs*1000.0);
+  fclose (archivo);
+  */
+  delete []array;
+
+  return 0;
+}
+
+// Algoritmo de ordenamiento Radix Sort
+void radixSort(int *array, int n) {
+
+  int m = max(array, n);
+
+  for (int exp = 1; m/exp > 0; exp*=10) {
+    sorting(array, n, exp);
+  }
+}
+
+// Función para encontrar el max de un arreglo
+int max(int *array, int n) {
+
+  int max = array[0];
+
+  for (int i = 1; i < n; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  return max;
+}
+
+// Función para ordenar por base 10 y exponente (10, 10^2, 10^3,...)
+void sorting(int *array, int n, int exp){
+
+  int output[n];
+  int i, count[10] = {0};
+
+  for (i = 0; i < n; i++) {
+    count[(array[i]/exp)%10]++;
+  }
+
+  for (i = 1; i < 10; i++) {
+    count[i]+=count[i-1];
+  }
+
+  for (i = n-1; i >= 0; i--) {
+    output[count[(array[i]/exp)%10]-1] = array[i];
+    count[(array[i]/exp)%10]--;
+  }
+
+  for (i = 0; i < n; i++) {
+    array[i] = output[i];
+  }
+}
---
+++ residuo.cpp
@@ -0,0 +1,99 @@
+/**
+  Diego Alejandro Rodriguez Ayala - 20142020068
+  Practica para estudiar el algoritmo de ordenamiento por residuo y su comportamiento
+  Ademas se calcula tiempo de ejecución del algoritmo para compararlo despues con el t(n) teorico.
+**/
+#include <stdio.h>
+#include "sorting_algorithms.h"
+#include <time.h>
+//#include <windows.h>
+
+inline unsigned bits(int k, int j, int x);
+void residuo(int *array, int left, int right, int bit);
+//double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b);
+
+int main(int argc, char const *argv[]) {
+
+  int *array, n;
+  /*
+  double secs = 0;
+  LARGE_INTEGER t_ini, t_fin;
+
+  FILE *archivo;
+  archivo = fopen("tiempo.txt", "a");
+  */
+  printf("Ingrese el valor de N: ");
+  scanf("%i", &n);
+
+  array = new int[n];
+
+  srand(time(0));
+
+  for (int i = 0; i < n; i++) {
+    array[i] = rand()%7+8; // Para 4 bits
+    //array[i] = rand()%128+128; // Para 8 bits
+    //array[i] = rand()%32808+32727; // Para 16 bits
+  }
+
+  printf("\nArreglo:\n");
+  printArray(array, n);
+
+  // Mide tiempo de ejecución entre &t_ini y &t_fin
+  //QueryPerformanceCounter(&t_ini);
+  residuo(array, 0, n-1, 3); // Para 4 bits
+  //residuo(array, 0, n-1, 7); // Para 8 bits
+  //residuo(array, 0, n-1, 15); // Para 16 bits
+  //QueryPerformanceCounter(&t_fin);
+
+  printf("\nArray Ordenado:\n");
+  printArray(array, n);
+  /*
+  secs = performancecounter_diff(&t_fin, &t_ini);
+  printf("\nTime: %.16g milliseconds\n", secs*1000.0);
+
+  fprintf(archivo, "\n%.16g", secs*1000.0);
+  fclose (archivo);
+  */
+  delete []array;
+
+  return 0;
+}
+
+// Algoritmo de ordenamiento por residuo (bit: cantidad de bits a bits a evaluar)
+void residuo(int *array, int left, int right, int bit) {
+
+  int i, j;
+
+  if ((right > left) && (bit >= 0)) {
+    i = left;
+    j = right;
+    while (j != i) {
+      while ((!bits(bit, 1, array[i])) && (i < j)) {
+        i++;
+      }
+      while ((bits(bit, 1, array[j])) && (j > i)) {
+        j--;
+      }
+      exchange(array, i, j);
+    }
+    if (!bits(bit, 1, array[i])) {
+      j++;
+    }
+    residuo(array, left, j-1, bit-1);
+    residuo(array, j, right, bit-1);
+  }
+}
+
+inline unsigned bits(int k, int j, int x) {
+  return (x>>k) & ~(~0<<j);
+}
+/*
+// Funcion para medir tiempo de ejecución (windows) - agregar include <windows.h>
+double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b) {
+
+  LARGE_INTEGER freq;
+  QueryPerformanceFrequency(&freq);
+
+  return (double)(a->QuadPart - b->QuadPart) / (double)freq.QuadPart;
+}
+*/
